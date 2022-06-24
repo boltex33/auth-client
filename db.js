ec7f72212433e54c6a2e267f97220368e5e0b937
boltex33 <laca_rey@yahoo.com>
2022-06-24 21:06:55 +0300

    ready for deployment

--- db.js
+++ db.js
@@ -1,10 +1,20 @@
 const Pool = require("pg").Pool;
-const pool = new Pool({
-  user: "postgres",
-  password: "nughicestiparola69yasuo",
-  host: "localhost",
-  port: 5432,
-  database: "jwttutorial",
-});
+require("dotenv").config();
+
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+};
+
+const proConfig = {
+  connectionString: process.env.DATABASE_URL, //heroku addons
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+//process.env.PORT
 
 //middleware
 
 app.use(express.json()); //req.body
 app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  //server static content
+  //mpm run build
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
 
 //Routes//
 
@@ -14,6 +24,10 @@ app.use(cors());
 app.use("/auth", require("./routes/jwtAuth"));
 app.use("/dashboard", require("./routes/dashboard"));
 
-app.listen(5000, () => {
-  console.log("server is running on port 5000");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
 });
